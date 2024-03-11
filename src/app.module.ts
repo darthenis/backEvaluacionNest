235ff467ccc54e93b5da2e78146c709825fd2a0a
darthenis <emi.acevedo.letras@gmail.com>
2024-03-11 14:22:02 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI)
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
